131341e0bdc9a0f0732daa3961c50d71a8b2d4a9
Kalinin <9776020@mail.ru>
2023-02-26 18:38:26 +0300

    HW_Task_1_3

---
+++ Seminar1_25_02/HW/1_3_HW/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine()!);
+int start_count = 2;
+if (a <= 1) Console.WriteLine("Error enter correct number");
+else
+{
+while(start_count <= a)
+{
+  Console.Write(start_count + " ");
+  start_count = start_count + 2;
+}}
