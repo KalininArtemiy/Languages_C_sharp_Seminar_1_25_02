9d6c4a1d729b7ed30cbdbee17dcf7bad95eaa3fb
Kalinin <9776020@mail.ru>
2023-02-26 17:40:11 +0300

    HW_Task_1_1

---
+++ Seminar1_25_02/HW/1_1_HW/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine())!;
+int b = int.Parse(Console.ReadLine())!;
+if (a > b) Console.WriteLine("Max is " + a);
+else Console.WriteLine("Max is " + b);
